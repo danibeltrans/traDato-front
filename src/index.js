616e1070d6707f44017ec4a81c1dca53ead1f1a9
Emil Hernandez <emilhernandez92@gmail.com>
2018-05-08 19:17:37 -0500

    added style-component and ant design

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
+import 'antd/dist/antd.css';
 import App from 'Containers/Routes';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
